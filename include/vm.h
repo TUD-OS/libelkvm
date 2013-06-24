3310c030ead82abd041cd19d7e32826053a8e24f
Florian Pester <florian.pester@tu-dresden.de>
2013-06-24 11:53:07 +0200

    add func to clean vm

--- include/vm.h
+++ include/vm.h
@@ -3,7 +3,8 @@
 #include <libelf.h>
 
 #define VM_MODE_X86    1
-#define VM_MODE_X86_64 2
+#define VM_MODE_PAGING 2
+#define VM_MODE_X86_64 3
 
 struct kvm_vm {
 	int fd;
@@ -13,6 +14,7 @@ struct kvm_vm {
 
 /*
 	Create a new VM, with the given mode, cpu count and memory
+	Return 0 on success, -1 on error
 */
 int kvm_vm_create(struct kvm_vm *, int, int, int);
 
@@ -25,3 +27,5 @@ int kvm_vm_load_binary(struct kvm_vm *, const char *);
 	Writes the state of the VM to a given file descriptor
 */
 void kvm_dump_vm(struct kvm_vm *, int);
+
+int kvm_vm_destroy(struct kvm_vm *);
