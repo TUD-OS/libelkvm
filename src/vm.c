dd7177ecb894ce1a5379e1188ae14c1397044878
Florian Pester <florian.pester@tu-dresden.de>
2013-07-01 12:46:18 +0200

    actually map chunks in vm_init

--- src/vm.c
+++ src/vm.c
@@ -27,7 +27,17 @@ int kvm_vm_create(struct kvm_opts *opts, struct kvm_vm *vm, int mode, int cpus, 
 		return err;
 	}
 
+	err = kvm_vm_map_chunk(vm, &vm->pager.system_chunk);
+	if(err) {
+		return err;
+	}
+
 	err = kvm_pager_create_mem_chunk(&vm->pager, memory_size, ELKVM_USER_CHUNK_OFFSET);
+	if(err) {
+		return err;
+	}
+
+	err = kvm_vm_map_chunk(vm, vm->pager.other_chunks->chunk);
 	if(err) {
 		return err;
 	}
@@ -102,5 +112,19 @@ int kvm_cleanup(struct kvm_opts *opts) {
 
 int kvm_vm_map_chunk(struct kvm_vm *vm, struct kvm_userspace_memory_region *chunk) {
 	int err = ioctl(vm->fd, KVM_SET_USER_MEMORY_REGION, chunk);
+//	if(err) {
+//		long sz = sysconf(_SC_PAGESIZE);
+//		printf("Could not set memory region\n");
+//		printf("Error No: %i Msg: %s\n", errno, strerror(errno));
+//		printf("Pagesize is: %li\n", sz);
+//		printf("Here are some sanity checks that are applied in kernel:\n");
+//		int ms = chunk->memory_size & (sz-1);
+//		int pa = chunk->guest_phys_addr & (sz-1);
+//		int ua = chunk->userspace_addr & (sz-1);
+//		printf("memory_size & (PAGE_SIZE -1): %i\n", ms);
+//		printf("guest_phys_addr & (PAGE_SIZE-1): %i\n", pa);
+//		printf("userspace_addr & (PAGE_SIZE-1): %i\n", ua);
+//		printf("TODO verify write access\n");
+//	}
 	return err;
 }
