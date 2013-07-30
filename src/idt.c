2b08c9530069cd8ba188ab4314f22cd6eccccdb9
Florian Pester <florian.pester@tu-dresden.de>
2013-07-30 17:42:13 +0200

    load default handler, fix limit, dump idt

--- src/idt.c
+++ src/idt.c
@@ -1,43 +1,55 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+
 #include <elkvm.h>
 #include <idt.h>
 #include <pager.h>
 #include <vcpu.h>
 
 int elkvm_idt_setup(struct kvm_vm *vm) {
+
+	uint64_t default_offset;
+	int err = elkvm_idt_load_default_handler(vm, &default_offset);
+
 	/* for now fill the idt with all 256 entries empty */
 	for(int i = 0; i < 256; i++) {
-		uint64_t offset;
+		uint64_t offset = default_offset;
 		struct kvm_idt_entry *entry = vm->region[MEMORY_REGION_IDT].host_base_p + 
 			i * sizeof(struct kvm_idt_entry);
-		switch(i) {
-//			case IDT_ENTRY_PF:
-//					;
-//					uint64_t host_pf_handler = load_pfhandler(ram, size);
-//					offset = host_pf_handler - (uint64_t)ram;
-//					printf("SETTING pf_handler at: 0x%lx\n", offset);
-//				break;
-			default:
-				offset = 0x0;
-				break;
-		}
+		//switch(i) {
+		//		case IDT_ENTRY_PF:
+		//				;
+		//				uint64_t host_pf_handler = load_pfhandler(ram, size);
+		//				offset = host_pf_handler - (uint64_t)ram;
+		//				printf("SETTING pf_handler at: 0x%lx\n", offset);
+		//			break;
+		//	default:
+		//		offset = 0x0;
+		//		break;
+		//}
 		entry->offset1 = (uint16_t)((offset << 48) >> 48);
 		entry->offset2 = (uint16_t)((offset << 32) >> 48);
 		entry->offset3 = (uint32_t)(offset >> 32);
 
 		entry->selector = 0x1000;
 		entry->idx = 0x0;
-		entry->flags = INTERRUPT_ENTRY_PRESENT | IT_INTERRUPT_GATE;
+		entry->flags = INTERRUPT_ENTRY_PRESENT | IT_TRAP_GATE;
 		entry->reserved = 0x0;
 	}
 
 
 	/* create a page for the idt */
-	int err = kvm_pager_create_mapping(&vm->pager, 
+	err = kvm_pager_create_mapping(&vm->pager, 
 			vm->region[MEMORY_REGION_IDT].host_base_p, 
 			vm->region[MEMORY_REGION_IDT].guest_virtual);
 	if(err) {
 		return err;
 	}
+
+	elkvm_idt_dump(vm);
 
 	/* set the idtr accordingly */
 	struct kvm_vcpu *vcpu = vm->vcpus->vcpu;
@@ -47,7 +59,7 @@ int elkvm_idt_setup(struct kvm_vm *vm) {
 	}
 
 	vcpu->sregs.idt.base = vm->region[MEMORY_REGION_IDT].guest_virtual;
-	vcpu->sregs.idt.limit = 0x1000;
+	vcpu->sregs.idt.limit = 0xFFF;
 
 	err = kvm_vcpu_set_regs(vcpu);
 
