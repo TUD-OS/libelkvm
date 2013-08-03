1c8b9ea57a9b484a3aaa2af55e4778960a1df09f
Florian Pester <florian.pester@tu-dresden.de>
2013-08-03 18:29:00 +0200

    define magic flag and access bytes

--- include/gdt.h
+++ include/gdt.h
@@ -9,11 +9,15 @@ struct elkvm_gdt_entry {
 	uint8_t base3;
 };
 
-#define GDT_SEGMENT_WRITEABLE 2
-#define GDT_SEGMENT_EXECUTABLE 8
-#define GDT_BIT_SET 16
-#define GDT_SEGMENT_PRESENT 128
-#define GDT_SEGMENT_PROTECTED 64
+#define GDT_SEGMENT_WRITEABLE          2
+#define GDT_SEGMENT_READABLE           2
+#define GDT_SEGMENT_DIRECTION_BIT      4
+#define GDT_SEGMENT_EXECUTABLE         8
+#define GDT_SEGMENT_BIT	              16
+#define GDT_SEGMENT_PRIVILEDGE_USER   96
+#define GDT_SEGMENT_PRESENT          128
+#define GDT_SEGMENT_LONG              32
+#define GDT_SEGMENT_PROTECTED_32      64
 #define GDT_SEGMENT_PAGE_GRANULARITY 128
 
 int elkvm_gdt_setup(struct kvm_vm *);
