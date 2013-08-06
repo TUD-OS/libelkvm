d99282e191365da62ec80201a28043a4bda71018
Florian Pester <florian.pester@tu-dresden.de>
2013-08-06 14:25:18 +0200

    add handler funcs for vm exit, syscalls

---
+++ include/syscall.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <vcpu.h>
+
+/*
+ * \brief check if the shutdown reason is a call from the guest
+ * manager to the host
+ * returns 1 if the vm is to keep running
+ * returns 0 if the vm has had an error (#TF) and should stop running
+ */
+int elkvm_handle_vm_shutdown(struct kvm_vcpu *);
+
+/*
+ * \brief check what kind of syscall has been made by the guest
+ * and call the appropriate handler func in the userspace binary
+ */
+int elkvm_handle_syscall(struct kvm_vcpu *);
---
+++ src/syscall.c
@@ -0,0 +1,10 @@
+
+#include <syscall.h>
+
+int elkvm_handle_vm_shutdown(struct kvm_vcpu *vcpu) {
+	return -1;
+}
+
+int elkvm_handle_syscall(struct kvm_vcpu *vcpu) {
+	return -1;
+}
