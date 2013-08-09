a264de15e4012ed7abe0b96bad438ddbe56f2078
Florian Pester <florian.pester@tu-dresden.de>
2013-08-09 16:19:02 +0200

    add tests for dynamic region manager

--- test/test_region.c
+++ test/test_region.c
@@ -18,12 +18,107 @@ void setup_region() {
 
 }
 
+void setup_region_tree() {
+	int err = posix_memalign(&region_vm.root_region.host_base_p, 0x1000, 0x400000);
+	assert(err == 0);
+
+	region_vm.root_region.guest_virtual = 0x0;
+	region_vm.root_region.region_size = 0x400000;
+	region_vm.root_region.grows_downward = 0;
+	region_vm.root_region.used = 1;
+
+	struct elkvm_memory_region *lc = malloc(sizeof(struct elkvm_memory_region));
+	region_vm.root_region.lc = lc;
+	lc->guest_virtual = 0x0;
+	lc->host_base_p = region_vm.root_region.host_base_p;
+	lc->region_size = region_vm.root_region.region_size / 2;
+	lc->used = 0;
+	lc->lc = lc->rc = NULL;
+	region_vm.root_region.lc = lc;
+
+	struct elkvm_memory_region *rc = malloc(sizeof(struct elkvm_memory_region));
+	region_vm.root_region.rc = rc;
+	rc->guest_virtual = 0x0;
+	rc->host_base_p = region_vm.root_region.host_base_p + lc->region_size;
+	rc->region_size = region_vm.root_region.region_size / 2;
+	rc->used = 0;
+	rc->lc = rc->rc = NULL;
+	region_vm.root_region.rc = rc;
+}
+
 void teardown_region() {
 	free(region_vm.root_region.host_base_p);
 }
 
+void teardown_region_tree() {
+	free(region_vm.root_region.lc);
+	free(region_vm.root_region.rc);
+	free(region_vm.root_region.host_base_p);
+}
+
+START_TEST(test_region_find_root) {
+	region_vm.root_region.used = 0;
+
+	struct elkvm_memory_region *r = elkvm_region_find(&region_vm.root_region,
+			region_vm.root_region.region_size);
+	ck_assert_ptr_eq(r, &region_vm.root_region);
+}
+END_TEST
+
+START_TEST(test_region_find_left) {
+	region_vm.root_region.rc->used = 1;
+
+	struct elkvm_memory_region *region = elkvm_region_find(&region_vm.root_region, region_vm.root_region.region_size / 2);
+	ck_assert_ptr_eq(region, region_vm.root_region.lc);
+}
+END_TEST
+
+START_TEST(test_region_find_right) {
+	region_vm.root_region.lc->used = 1;
+
+	struct elkvm_memory_region *region = elkvm_region_find(&region_vm.root_region, region_vm.root_region.region_size / 2);
+	ck_assert_ptr_eq(region, region_vm.root_region.rc);
+}
+END_TEST
+
+START_TEST(test_region_find_smaller) {
+	uint64_t size = region_vm.root_region.region_size / 4;
+	struct elkvm_memory_region *region = elkvm_region_find(&region_vm.root_region, size);
+	ck_assert_ptr_eq(region, region_vm.root_region.lc->lc);
+	ck_assert_int_eq(region->region_size, size);
+
+	free(region);
+}
+END_TEST
+
+START_TEST(test_region_find_full) {
+	region_vm.root_region.lc->used = 1;
+	region_vm.root_region.rc->used = 1;
+
+	struct elkvm_memory_region *region = elkvm_region_find(&region_vm.root_region, 0x1000);
+	ck_assert_ptr_eq(region, NULL);
+	
+}
+END_TEST
+
+START_TEST(test_region_find_different_size) {
+	uint64_t size = (region_vm.root_region.region_size / 2) - 0x1000;
+	struct elkvm_memory_region *region = elkvm_region_find(&region_vm.root_region, size);
+	ck_assert_ptr_eq(region, region_vm.root_region.lc);
+}
+END_TEST
+
+START_TEST(test_region_split_invalid) {
+	region_vm.root_region.used = 1;
+	int err = elkvm_region_split(&region_vm.root_region);
+	ck_assert_int_eq(err, -1);
+	ck_assert_ptr_eq(region_vm.root_region.lc, NULL);
+	ck_assert_ptr_eq(region_vm.root_region.rc, NULL);
+}
+END_TEST
+
 START_TEST(test_region_split) {
-	int err = elkvm_region_split(&region_vm, &region_vm.root_region);
+	int err = elkvm_region_split(&region_vm.root_region);
 	ck_assert_int_eq(err, 0);
 	ck_assert_int_eq(region_vm.root_region.used, 1);
 	ck_assert_ptr_ne(region_vm.root_region.lc, NULL);
@@ -62,13 +157,13 @@ START_TEST(test_region_create_quarter_size) {
 	ck_assert_ptr_ne(new_region, &region_vm.root_region);
 	ck_assert_int_eq(region_vm.root_region.used, 1);
 	ck_assert_ptr_ne(region_vm.root_region.lc, NULL);
-	ck_assert_ptr_eq(region_vm.root_region.rc, NULL);
+	ck_assert_ptr_ne(region_vm.root_region.rc, NULL);
 
 	struct elkvm_memory_region *lc = region_vm.root_region.lc;
 	ck_assert_ptr_ne(new_region, lc);
 	ck_assert_int_eq(lc->used, 1);
 	ck_assert_ptr_ne(lc->lc, NULL);
-	ck_assert_ptr_eq(lc->rc, NULL);
+	ck_assert_ptr_ne(lc->rc, NULL);
 
 	lc = lc->lc;
 	ck_assert_ptr_eq(new_region, lc);
@@ -86,13 +181,13 @@ START_TEST(test_region_create_different_size) {
 	ck_assert_ptr_ne(new_region, &region_vm.root_region);
 	ck_assert_int_eq(region_vm.root_region.used, 1);
 	ck_assert_ptr_ne(region_vm.root_region.lc, NULL);
-	ck_assert_ptr_eq(region_vm.root_region.rc, NULL);
+	ck_assert_ptr_ne(region_vm.root_region.rc, NULL);
 
 	struct elkvm_memory_region *lc = region_vm.root_region.lc;
 	ck_assert_ptr_ne(new_region, lc);
 	ck_assert_int_eq(lc->used, 1);
 	ck_assert_ptr_ne(lc->lc, NULL);
-	ck_assert_ptr_eq(lc->rc, NULL);
+	ck_assert_ptr_ne(lc->rc, NULL);
 
 	lc = lc->lc;
 	ck_assert_ptr_eq(new_region, lc);
@@ -115,7 +210,18 @@ END_TEST
 Suite *region_suite() {
 	Suite *s = suite_create("Dynamic Region Manager");
 
+	TCase *tc_region_find = tcase_create("Region Find");
+	tcase_add_test(tc_region_find, test_region_find_root);
+	tcase_add_test(tc_region_find, test_region_find_left);
+	tcase_add_test(tc_region_find, test_region_find_right);
+	tcase_add_test(tc_region_find, test_region_find_smaller);
+	tcase_add_test(tc_region_find, test_region_find_full);
+	tcase_add_test(tc_region_find, test_region_find_different_size);
+	tcase_add_checked_fixture(tc_region_find, setup_region_tree, teardown_region_tree);
+	suite_add_tcase(s, tc_region_find);
+
 	TCase *tc_region_split = tcase_create("Region Split");
+	tcase_add_test(tc_region_split, test_region_split_invalid);
 	tcase_add_test(tc_region_split, test_region_split);
 	tcase_add_checked_fixture(tc_region_split, setup_region, teardown_region);
 	suite_add_tcase(s, tc_region_split);
