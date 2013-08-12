b88fbbd715414dd8ab606fbbe167a1731bbc2f2f
Florian Pester <florian.pester@tu-dresden.de>
2013-08-12 17:01:15 +0200

    dump idt, isr

--- src/idt.c
+++ src/idt.c
@@ -69,35 +69,35 @@ int elkvm_idt_setup(struct kvm_vm *vm, struct elkvm_flat *default_handler) {
 }
 
 void elkvm_idt_dump(struct kvm_vm *vm) {
-	//struct kvm_idt_entry *entry = 
-	//	(struct kvm_idt_entry *)vm->region[MEMORY_REGION_IDT].host_base_p;
-	//printf("\n Interrupt Descriptor Table:\n");
-	//printf(" ---------------------------\n\n");
-	//printf("Vector\tSelector\tOffset\tidx\tflags\n");
-	//for(int i = 0; i < 256; i++) {
-	//	printf("%i\t0x%4x\t0x%016lx\t%u\t%u\n", i, entry->selector,
-	//			idt_entry_offset(entry), entry->idx, 
-	//			entry->flags);
-	//}
+	struct kvm_idt_entry *entry = 
+		(struct kvm_idt_entry *)vm->idt_region->host_base_p;
+	printf("\n Interrupt Descriptor Table:\n");
+	printf(" ---------------------------\n\n");
+	printf("Vector\tSelector\tOffset\tidx\tflags\n");
+	for(int i = 0; i < 256; i++) {
+		printf("%i\t0x%4x\t0x%016lx\t%u\t%u\n", i, entry->selector,
+				idt_entry_offset(entry), entry->idx, 
+				entry->flags);
+	}
 }	
 
 void elkvm_idt_dump_isr(struct kvm_vm *vm, int iv) {
-	//struct kvm_vcpu *vcpu = vm->vcpus->vcpu;
-	//struct kvm_idt_entry *entry = vm->region[MEMORY_REGION_IDT].host_base_p +
-	//	iv * sizeof(struct kvm_idt_entry);
-	//uint64_t guest_isr = idt_entry_offset(entry);
-	//printf("guest_isr: 0x%lx\n", guest_isr);
-	//char *isr = kvm_pager_get_host_p(&vm->pager, guest_isr);
-	//printf("isr: %p\n", isr);
+	struct kvm_vcpu *vcpu = vm->vcpus->vcpu;
+	struct kvm_idt_entry *entry = vm->idt_region->host_base_p +
+		iv * sizeof(struct kvm_idt_entry);
+	uint64_t guest_isr = idt_entry_offset(entry);
+	printf("guest_isr: 0x%lx\n", guest_isr);
+	char *isr = kvm_pager_get_host_p(&vm->pager, guest_isr);
+	printf("isr: %p\n", isr);
 
-	//ud_set_input_buffer(&vcpu->ud_obj, isr, 9);
+	ud_set_input_buffer(&vcpu->ud_obj, isr, 9);
 
-	//printf("\n ISR Code for Interrupt Vector %3i:\n", iv);
-	//printf(  " ----------------------------------\n");
-	//while(ud_disassemble(&vcpu->ud_obj)) {
-	//	printf(" %s\n", ud_insn_asm(&vcpu->ud_obj));
-	//}
-	//printf("\n");
+	printf("\n ISR Code for Interrupt Vector %3i:\n", iv);
+	printf(  " ----------------------------------\n");
+	while(ud_disassemble(&vcpu->ud_obj)) {
+		printf(" %s\n", ud_insn_asm(&vcpu->ud_obj));
+	}
+	printf("\n");
 
-	//return;
+	return;
 }
