b37ef309812558c8b4a513cd5d4323504c689d1b
Florian Pester <florian.pester@tu-dresden.de>
2013-08-21 16:03:35 +0200

    region create now creates new memory chunks if there is no guest space left

--- src/region.c
+++ src/region.c
@@ -5,16 +5,29 @@
 #include <region.h>
 
 struct elkvm_memory_region *elkvm_region_create(struct kvm_vm *vm, uint64_t size) {
-	struct elkvm_memory_region_list *current_root = &vm->root_region;
-  struct elkvm_memory_region *current = NULL;
+	struct elkvm_memory_region_list *current_root = vm->root_region;
+  struct elkvm_memory_region *current = current_root->data;
 
   do {
     current = elkvm_region_find(current, size);
-  } while((current == NULL) || (current_root = current_root->next) == NULL);
+  } while((current == NULL) && !((current_root = current_root->next) == NULL));
 
   if(current == NULL) {
     /* TODO get a new memory chunk and add that to the list of root regions */
-    int err = kvm_pager_create_mem_chunk(&vm->pager, ELKVM_SYSTEM_MEMGROW);
+    void *chunk_p;
+    int err = kvm_pager_create_mem_chunk(&vm->pager, &chunk_p, ELKVM_SYSTEM_MEMGROW);
+    if(err) {
+      return NULL;
+    }
+    current = elkvm_region_alloc(chunk_p, ELKVM_SYSTEM_MEMGROW, 0);
+    if(current == NULL) {
+      return NULL;
+    }
+    current_root = elkvm_region_list_prepend(vm, current);
+    if(current_root == NULL) {
+      return NULL;
+    }
+
   }
 
 	current->used = 1;
