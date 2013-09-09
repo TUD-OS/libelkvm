488db07a9ea1e4c2f2496e552b043e4cbdb5a915
Florian Pester <florian.pester@tu-dresden.de>
2013-09-09 12:37:02 +0200

    remove handle_vm_shutdown

--- include/syscall.h
+++ include/syscall.h
@@ -4,14 +4,6 @@
 #include <asm/unistd_64.h>
 
 /*
- * \brief check if the shutdown reason is a call from the guest
- * manager to the host
- * returns 1 if the vm is to keep running
- * returns 0 if the vm has had an error (#TF) and should stop running
- */
-int elkvm_handle_vm_shutdown(struct kvm_vm *, struct kvm_vcpu *);
-
-/*
  * \brief check what kind of syscall has been made by the guest
  * and call the appropriate handler func in the userspace binary
  */
