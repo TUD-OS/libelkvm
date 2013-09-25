ac7b03099f39344b3458f31815f3d5e435e9865c
Florian Pester <florian.pester@tu-dresden.de>
2013-09-25 15:07:18 +0200

    don't get/set regs on push/pop

--- src/stack.c
+++ src/stack.c
@@ -7,40 +7,27 @@
 #include <vcpu.h>
 
 uint64_t elkvm_popq(struct kvm_vm *vm, struct kvm_vcpu *vcpu) {
-	int err = kvm_vcpu_get_regs(vcpu);
-	assert(err == 0);
-
 	uint64_t *host_p = kvm_pager_get_host_p(&vm->pager, vcpu->regs.rsp);
   assert(host_p != NULL);
 
 	//vm->region[MEMORY_REGION_STACK].region_size -= 0x8;
 	vcpu->regs.rsp += 0x8;
-	err = kvm_vcpu_set_regs(vcpu);
 	assert(err == 0);
 
 	return *host_p;
 }
 
 uint32_t elkvm_popd(struct kvm_vm *vm, struct kvm_vcpu *vcpu) {
-  int err = kvm_vcpu_get_regs(vcpu);
-  assert(err == 0);
-
   uint32_t *host_p = kvm_pager_get_host_p(&vm->pager, vcpu->regs.rsp);
   assert(host_p != NULL);
 
   vcpu->regs.rsp += 0x4;
-  err = kvm_vcpu_set_regs(vcpu);
   assert(err == 0);
 
   return *host_p;
 }
 
 int elkvm_pushq(struct kvm_vm *vm, struct kvm_vcpu *vcpu, uint64_t val) {
-	int err = kvm_vcpu_get_regs(vcpu);
-	if(err < 0) {
-		return err;
-	}
-
 	//vm->region[MEMORY_REGION_STACK].region_size += 0x8;
 	vcpu->regs.rsp -= 0x8;
 
@@ -56,7 +43,6 @@ int elkvm_pushq(struct kvm_vm *vm, struct kvm_vcpu *vcpu, uint64_t val) {
 	}
 	*host_p = val;
 
-	err = kvm_vcpu_set_regs(vcpu);
 	return err;
 }
 
