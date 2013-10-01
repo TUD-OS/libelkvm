a549dca1a6cecf63a8e57d921f09de3c9e4cd29b
Florian Pester <florian.pester@tu-dresden.de>
2013-10-01 17:32:07 +0200

    dump vm on mmio exit

--- src/vcpu.c
+++ src/vcpu.c
@@ -521,6 +521,7 @@ int kvm_vcpu_loop(struct kvm_vcpu *vcpu) {
 
 		if(	vcpu->singlestep ||
         vcpu->run_struct->exit_reason == KVM_EXIT_DEBUG ||
+        vcpu->run_struct->exit_reason == KVM_EXIT_MMIO ||
 				vcpu->run_struct->exit_reason == KVM_EXIT_SHUTDOWN) {
 			kvm_vcpu_dump_regs(vcpu);
       elkvm_dump_stack(vcpu->vm, vcpu);
