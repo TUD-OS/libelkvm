92826d911f9626b482f7693f4de8342ae3fd4452
Florian Pester <florian.pester@tu-dresden.de>
2013-10-15 15:57:44 +0200

    add impl for elkvm_debug_breakpoint

--- src/debug.c
+++ src/debug.c
@@ -16,8 +16,21 @@ int elkvm_debug_singlestep(struct kvm_vcpu *vcpu) {
   return elkvm_set_guest_debug(vcpu);
 }
 
-int elkvm_debug_breakpoint(struct kvm_vcpu *vcpu, uint64_t rip) {
-  return -1;
+int elkvm_debug_breakpoint(struct kvm_vm *vm, struct kvm_vcpu *vcpu, uint64_t rip) {
+  static const uint8_t int3 = 0xcc;
+
+  vcpu->debug.control |= KVM_GUESTDBG_ENABLE | KVM_GUESTDBG_USE_SW_BP;
+
+  uint8_t *host_p = (uint8_t *)kvm_pager_get_host_p(&vm->pager, rip);
+  struct elkvm_sw_bp *bp = malloc(sizeof(struct elkvm_sw_bp));
+  bp->guest_virtual_addr = rip;
+  bp->orig_inst = *host_p;
+  bp->count = 0;
+
+  *host_p = int3;
+  list_push_front(vcpu->breakpoints, bp);
+
+  return elkvm_set_guest_debug(vcpu);
 }
 
 int elkvm_set_guest_debug(struct kvm_vcpu *vcpu) {
