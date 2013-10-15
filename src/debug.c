e11c522fc6ab0ee445bda71a6f0db08d4a2845ba
Florian Pester <florian.pester@tu-dresden.de>
2013-10-15 16:58:29 +0200

    add basic debug shell

--- src/debug.c
+++ src/debug.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <errno.h>
+#include <stdio.h>
 #include <sys/ioctl.h>
 
 #include <elkvm.h>
@@ -9,19 +10,26 @@ int elkvm_handle_debug(struct kvm_vm *vm, struct kvm_vcpu *vcpu) {
   uint8_t *host_p = (uint8_t *)kvm_pager_get_host_p(&vm->pager, vcpu->regs.rip);
   assert(host_p != NULL);
 
-  struct elkvm_sw_bp *bp;
+  struct elkvm_sw_bp *bp = NULL;
   list_each(vcpu->breakpoints, p) {
     if(p->guest_virtual_addr == vcpu->regs.rip) {
       bp = p;
     }
   }
 
-  *host_p = bp->orig_inst;
-  bp->count++;
+  if(bp != NULL) {
+    *host_p = bp->orig_inst;
+    bp->count++;
 
-  printf("Hit Breakpoint %p the %ith time\n", bp, bp->count);
+    printf("Hit Breakpoint %p the %ith time\n", bp, bp->count);
+  }
 
- return -1;
+  int abort = elkvm_debug_shell(vm);
+  if(abort) {
+    return -1;
+  }
+
+  return 0;
 }
 
 int elkvm_debug_enable(struct kvm_vcpu *vcpu) {
@@ -62,5 +70,45 @@ int elkvm_debug_breakpoint(struct kvm_vm *vm, struct kvm_vcpu *vcpu, uint64_t ri
 
 int elkvm_set_guest_debug(struct kvm_vcpu *vcpu) {
   return ioctl(vcpu->fd, KVM_SET_GUEST_DEBUG, &vcpu->debug);
+}
+
+int elkvm_debug_shell(struct kvm_vm *vm) {
+  char op;
+  struct kvm_vcpu *vcpu = elkvm_vcpu_get(vm, 0);
+
+  while(op != 'x') {
+    printf("\n");
+    printf(" Please enter what to do, h for help\n");
+    printf(" ===================================\n");
+    printf(" > ");
+    int items = scanf(" %c", &op);
+    printf("\n");
+    if(items == 0) {
+      continue;
+    }
+
+    switch(op) {
+      case 'h':
+        printf("\ta\tAbort execution and exit shell\n");
+        printf("\tx\tExit Shell and resume execution\n");
+        printf("\tr\tDump registers\n");
+        printf("\ts\tStep to the next instruction\n");
+        break;
+      case 'a':
+        return 1;
+      case 'r':
+        kvm_vcpu_dump_regs(vcpu);
+        break;
+      case 's':
+        elkvm_debug_singlestep(vcpu);
+        return 0;
+      default:
+        printf("Invalid command\n");
+        break;
+    }
+  }
+
+  return 0;
+
 }
 
