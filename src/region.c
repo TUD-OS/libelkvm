a144eec03a7166b49a44fd25710adedf520efd89
Florian Pester <florian.pester@tu-dresden.de>
2013-11-11 18:24:40 +0100

    fix region finding

--- src/region.c
+++ src/region.c
@@ -4,9 +4,14 @@
 #include <elkvm.h>
 #include <region.h>
 
-struct elkvm_memory_region *elkvm_region_create(struct kvm_vm *vm, uint64_t size) {
+struct elkvm_memory_region *elkvm_region_create(struct kvm_vm *vm, uint64_t req_size) {
 	struct elkvm_memory_region_list *current_root = vm->root_region;
   struct elkvm_memory_region *current = current_root->data;
+
+  uint64_t size = req_size;
+  if(req_size < 0x1000) {
+    size = 0x1000;
+  }
 
   do {
     current = current_root->data;
@@ -80,12 +85,14 @@ int elkvm_region_free(struct kvm_vm *vm, struct elkvm_memory_region *region) {
 
 struct elkvm_memory_region *
 	elkvm_region_find(struct elkvm_memory_region *region, uint64_t size) {
+    assert(size >= 0x1000);
+
 		if(size > region->region_size) {
 			return NULL;
 		}
 
 		uint64_t smaller_size = region->region_size / 2;
-		if((smaller_size < 0x1000) ||
+		if((smaller_size <= 0x1000) ||
         ((smaller_size < size) && (size <= region->region_size))) {
 			if(region->used == 0) {
 				return region;
