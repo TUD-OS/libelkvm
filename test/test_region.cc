aaf9b50bf49d001ab1867f808116e0b83d52aa06
Florian Pester <florian.pester@tu-dresden.de>
2014-05-30 14:04:43 +0200

    add some simple tests for the region helpers

---
+++ test/test_region.cc
@@ -0,0 +1,40 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "../src/region.h"
+
+namespace testing {
+  class RegionTest : public Test {
+    protected:
+      Elkvm::Region r;
+
+      RegionTest() :
+        r(reinterpret_cast<void *>(0xC0F000), 0x12000) {}
+      ~RegionTest() {}
+
+      virtual void SetUp() {}
+      virtual void TearDown() {}
+  };
+
+  TEST_F(RegionTest, test_contains_address) {
+    ASSERT_FALSE(r.contains_address(reinterpret_cast<void *>(0xc0efff)));
+    ASSERT_TRUE(r.contains_address(reinterpret_cast<void *>(0xc0f000)));
+    ASSERT_TRUE(r.contains_address(reinterpret_cast<void *>(0xc10000)));
+    ASSERT_TRUE(r.contains_address(reinterpret_cast<void *>(0xc20fff)));
+    ASSERT_FALSE(r.contains_address(reinterpret_cast<void *>(0xc21000)));
+  }
+
+  TEST_F(RegionTest, test_equality) {
+    ASSERT_TRUE(r == reinterpret_cast<void *>(0xc10000));
+    ASSERT_FALSE(r == reinterpret_cast<void *>(0xc21000));
+
+    Elkvm::Region r2 = r;
+    ASSERT_TRUE(r2 == r);
+
+//    ASSERT_TRUE(Elkvm::same_region(reinterpret_cast<void *>(0xC0F000),
+//                                   reinterpret_cast<void *>(0xC10000)));
+//    ASSERT_FALSE(Elkvm::same_region(reinterpret_cast<void *>(0xC0F000),
+//                                    reinterpret_cast<void *>(0xC21000)));
+  }
+
+}
