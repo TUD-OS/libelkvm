878961012d5610f7a9244af14dd9f1821018f57a
Florian Pester <florian.pester@tu-dresden.de>
2014-06-03 14:08:26 +0200

    add test for slice_begin

--- test/test_region.cc
+++ test/test_region.cc
@@ -31,10 +31,21 @@ namespace testing {
     Elkvm::Region r2 = r;
     ASSERT_TRUE(r2 == r);
 
+    //    XXX RM must be able to find these regions!
 //    ASSERT_TRUE(Elkvm::same_region(reinterpret_cast<void *>(0xC0F000),
 //                                   reinterpret_cast<void *>(0xC10000)));
 //    ASSERT_FALSE(Elkvm::same_region(reinterpret_cast<void *>(0xC0F000),
 //                                    reinterpret_cast<void *>(0xC21000)));
+  }
+
+  TEST_F(RegionTest, test_slicing) {
+    Elkvm::Region r2 = r.slice_begin(0x1000);
+    ASSERT_FALSE(r.contains_address(reinterpret_cast<void *>(0xC0F000)));
+    ASSERT_TRUE(r.contains_address(reinterpret_cast<void *>(0xC10000)));
+    ASSERT_TRUE(r2.contains_address(reinterpret_cast<void *>(0xC0F000)));
+    ASSERT_TRUE(r2.contains_address(reinterpret_cast<void *>(0xC0FFFF)));
+    ASSERT_FALSE(r2.contains_address(reinterpret_cast<void *>(0xC10000)));
+    ASSERT_TRUE(r.contains_address(reinterpret_cast<void *>(0xc20fff)));
   }
 
 }
