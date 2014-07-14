32f3e0cfe5c90f0f9e863c15e2dd2d3c99cdf98c
Florian Pester <florian.pester@tu-dresden.de>
2014-07-14 16:23:57 +0200

    add getter for debug mode and syscall handlers to vm_internals

--- include/elkvm/elkvm-internal.h
+++ include/elkvm/elkvm-internal.h
@@ -41,8 +41,14 @@ namespace Elkvm {
       std::shared_ptr<struct kvm_vcpu> get_vcpu(int num) const;
       int get_vmfd() const { return _vmfd; }
       std::shared_ptr<struct elkvm_flat> get_cleanup_flat() const;
+
+      const struct elkvm_handlers * get_handlers() const
+        { return _vm->syscall_handlers; }
+
       std::shared_ptr<struct sigaction> get_sig_ptr(unsigned sig) const;
       std::shared_ptr<struct kvm_vm> get_vm_ptr() const { return _vm; }
+
+      int debug_mode() const { return _vm->debug; }
 
       int set_entry_point(guestptr_t rip)
         { return kvm_vcpu_set_rip(cpus.front().get(), rip); }
