7a65509992d78c31c9b218789ffc70ce6ce1ffc5
Björn Döbel <bjoern.doebel@gmail.com>
2014-08-29 10:06:07 +0200

    ws

--- src/vm.cc
+++ src/vm.cc
@@ -50,57 +50,57 @@ Elkvm::kvm_vm *elkvm_vm_create(Elkvm::elkvm_opts *opts, int mode,
         run_struct_size,
         handlers,
         debug);
-	  Elkvm::VM &vmi = Elkvm::vmi.back();
+    Elkvm::VM &vmi = Elkvm::vmi.back();
 
-	  for(unsigned i = 0; i < cpus; i++) {
-		err = vmi.add_cpu(mode);
-			if(err) {
-		  errno = -err;
-				return NULL;
-			}
-	  }
+    for(unsigned i = 0; i < cpus; i++) {
+    err = vmi.add_cpu(mode);
+      if(err) {
+      errno = -err;
+        return NULL;
+      }
+    }
 
-	  Elkvm::ElfBinary bin(binary, vmi.get_region_manager(), vmi.get_heap_manager());
+    Elkvm::ElfBinary bin(binary, vmi.get_region_manager(), vmi.get_heap_manager());
 
-	  guestptr_t entry = bin.get_entry_point();
-	  err = vmi.set_entry_point(entry);
-	  assert(err == 0);
+    guestptr_t entry = bin.get_entry_point();
+    err = vmi.set_entry_point(entry);
+    assert(err == 0);
 
-	  Elkvm::Environment env(bin, vmi.get_region_manager());
+    Elkvm::Environment env(bin, vmi.get_region_manager());
 
-	  std::shared_ptr<struct kvm_vcpu> vcpu = vmi.get_vcpu(0);
+    std::shared_ptr<struct kvm_vcpu> vcpu = vmi.get_vcpu(0);
 
-	  /* gets and sets vcpu->regs */
-	  err = env.fill(opts, vcpu);
-	  assert(err == 0);
+    /* gets and sets vcpu->regs */
+    err = env.fill(opts, vcpu);
+    assert(err == 0);
 
-		err = elkvm_gdt_setup(*vmi.get_region_manager(), vcpu);
-	  assert(err == 0);
+    err = elkvm_gdt_setup(*vmi.get_region_manager(), vcpu);
+    assert(err == 0);
 
-		Elkvm::elkvm_flat idth;
-	  std::string isr_path(RES_PATH "/isr");
-		err = vmi.load_flat(idth, isr_path, 1);
-		if(err) {
-		if(err == -ENOENT) {
-		  printf("LIBELKVM: ISR shared file could not be found\n");
-		}
-		errno = -err;
-			return NULL;
-		}
+    Elkvm::elkvm_flat idth;
+    std::string isr_path(RES_PATH "/isr");
+    err = vmi.load_flat(idth, isr_path, 1);
+    if(err) {
+    if(err == -ENOENT) {
+      printf("LIBELKVM: ISR shared file could not be found\n");
+    }
+    errno = -err;
+      return NULL;
+    }
 
-		err = elkvm_idt_setup(*vmi.get_region_manager(), vcpu, &idth);
-	  assert(err == 0);
+    err = elkvm_idt_setup(*vmi.get_region_manager(), vcpu, &idth);
+    assert(err == 0);
 
-		Elkvm::elkvm_flat sysenter;
+    Elkvm::elkvm_flat sysenter;
   std::string sysenter_path(RES_PATH "/entry");
-	err = vmi.load_flat(sysenter, sysenter_path, 1);
-	if(err) {
+  err = vmi.load_flat(sysenter, sysenter_path, 1);
+  if(err) {
     if(err == -ENOENT) {
       printf("LIBELKVM: SYSCALL ENTRY shared file could not be found\n");
     }
     errno = -err;
-		return NULL;
-	}
+    return NULL;
+  }
 
   std::string sighandler_path(RES_PATH "/signal");
   auto sigclean = vmi.get_cleanup_flat();
@@ -113,12 +113,12 @@ Elkvm::kvm_vm *elkvm_vm_create(Elkvm::elkvm_opts *opts, int mode,
     return NULL;
   }
 
-	/*
-	 * setup the lstar register with the syscall handler
-	 */
-	err = kvm_vcpu_set_msr(vmi.get_vcpu(0).get(),
-			VCPU_MSR_LSTAR,
-			sysenter.region->guest_address());
+  /*
+   * setup the lstar register with the syscall handler
+   */
+  err = kvm_vcpu_set_msr(vmi.get_vcpu(0).get(),
+      VCPU_MSR_LSTAR,
+      sysenter.region->guest_address());
   assert(err == 0);
 
   std::shared_ptr<Elkvm::kvm_vm> vm = vmi.get_vm_ptr();
@@ -127,7 +127,7 @@ Elkvm::kvm_vm *elkvm_vm_create(Elkvm::elkvm_opts *opts, int mode,
     assert(err == 0);
   }
 
-	return vm.get();
+  return vm.get();
 }
 
 int elkvm_set_debug(Elkvm::kvm_vm *vm) {
@@ -136,33 +136,33 @@ int elkvm_set_debug(Elkvm::kvm_vm *vm) {
 }
 
 int elkvm_init(Elkvm::elkvm_opts *opts, int argc, char **argv, char **environ) {
-	opts->argc = argc;
-	opts->argv = argv;
-	opts->environ = environ;
+  opts->argc = argc;
+  opts->argv = argv;
+  opts->environ = environ;
 
-	opts->fd = open(KVM_DEV_PATH, O_RDWR);
-	if(opts->fd < 0) {
-		return -errno;
-	}
+  opts->fd = open(KVM_DEV_PATH, O_RDWR);
+  if(opts->fd < 0) {
+    return -errno;
+  }
 
-	int version = ioctl(opts->fd, KVM_GET_API_VERSION, 0);
-	if(version != KVM_EXPECT_VERSION) {
-		return -ENOPROTOOPT;
-	}
+  int version = ioctl(opts->fd, KVM_GET_API_VERSION, 0);
+  if(version != KVM_EXPECT_VERSION) {
+    return -ENOPROTOOPT;
+  }
 
-	opts->run_struct_size = ioctl(opts->fd, KVM_GET_VCPU_MMAP_SIZE, 0);
-	if(opts->run_struct_size <= 0) {
-		return -EIO;
-	}
+  opts->run_struct_size = ioctl(opts->fd, KVM_GET_VCPU_MMAP_SIZE, 0);
+  if(opts->run_struct_size <= 0) {
+    return -EIO;
+  }
 
-	return 0;
+  return 0;
 }
 
 int elkvm_cleanup(Elkvm::elkvm_opts *opts) {
-	close(opts->fd);
-	opts->fd = 0;
-	opts->run_struct_size = 0;
-	return 0;
+  close(opts->fd);
+  opts->fd = 0;
+  opts->run_struct_size = 0;
+  return 0;
 }
 
 int elkvm_chunk_remap(Elkvm::kvm_vm *vm, int num, uint64_t newsize) {
@@ -171,13 +171,13 @@ int elkvm_chunk_remap(Elkvm::kvm_vm *vm, int num, uint64_t newsize) {
   auto chunk = vmi.get_region_manager()->get_pager().get_chunk(num);
   chunk->memory_size = 0;
 
-	int err = ioctl(vm->fd, KVM_SET_USER_MEMORY_REGION, chunk.get());
+  int err = ioctl(vm->fd, KVM_SET_USER_MEMORY_REGION, chunk.get());
   assert(err == 0);
   free((void *)chunk->userspace_addr);
   chunk->memory_size = newsize;
   err = posix_memalign(((void **)&chunk->userspace_addr), ELKVM_PAGESIZE, chunk->memory_size);
   assert(err == 0);
-	err = ioctl(vm->fd, KVM_SET_USER_MEMORY_REGION, chunk.get());
+  err = ioctl(vm->fd, KVM_SET_USER_MEMORY_REGION, chunk.get());
   assert(err == 0);
   return 0;
 }
@@ -208,21 +208,21 @@ void elkvm_emulate_vmcall(struct kvm_vcpu *vcpu) {
 }
 
 int elkvm_dump_valid_msrs(Elkvm::elkvm_opts *opts) {
-	struct kvm_msr_list *list = reinterpret_cast<struct kvm_msr_list *>(
+  struct kvm_msr_list *list = reinterpret_cast<struct kvm_msr_list *>(
       malloc( sizeof(struct kvm_msr_list) + 255 * sizeof(uint32_t)));
-	list->nmsrs = 255;
+  list->nmsrs = 255;
 
-	int err = ioctl(opts->fd, KVM_GET_MSR_INDEX_LIST, list);
-	if(err < 0) {
-		free(list);
-		return -errno;
-	}
+  int err = ioctl(opts->fd, KVM_GET_MSR_INDEX_LIST, list);
+  if(err < 0) {
+    free(list);
+    return -errno;
+  }
 
-	for(unsigned i = 0; i < list->nmsrs; i++) {
-		printf("MSR: 0x%x\n", list->indices[i]);
-	}
-	free(list);
+  for(unsigned i = 0; i < list->nmsrs; i++) {
+    printf("MSR: 0x%x\n", list->indices[i]);
+  }
+  free(list);
 
-	return 0;
+  return 0;
 }
 
