4980e5d1401890efab072b36ede23cd2e74aa536
Björn Döbel <bjoern.doebel@gmail.com>
2015-01-06 22:03:45 +0100

    test runner

---
+++ test/integration/run_all.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+export ELKVM_BUILD=../../build
+
+return_check () {
+	if [ $? -ne 0 ]; then
+		echo "\033[31;1mFAILURE\033[0m"
+		exit $?;
+	fi
+	echo "\033[32mOK\033[0m"
+}
+
+print_msg () {
+	printf "\033[35m%30s\033[0m " "$1"
+}
+
+# Run expect test and check output
+#
+# Arguments:
+#	$1 -- descriptive message
+#   $2 -- name of expect script to run
+run_test () {
+	message=$1
+	cmd=$2
+	print_msg "$1"
+	expect $2
+	return_check
+}
+
+print_msg "expect installed?"
+which expect >/dev/null
+return_check
+
+run_test "ELKVM works?" 001-run.exp
