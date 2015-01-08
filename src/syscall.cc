f84567bc0c4cdcb23d66720ebf4107d7c0164981
Florian Pester <florian.pester@tu-dresden.de>
2015-01-08 13:24:57 +0100

    move mprotect impl to own file

--- src/syscall.cc
+++ src/syscall.cc
@@ -789,50 +789,6 @@ long elkvm_do_mmap(Elkvm::VM * vmi) {
   return mapping.guest_address();
 }
 
-long elkvm_do_mprotect(Elkvm::VM * vmi) {
-  std::shared_ptr<Elkvm::VCPU> vcpu = vmi->get_vcpu(0);
-
-  guestptr_t addr = 0;
-  CURRENT_ABI::paramtype len = 0;
-  CURRENT_ABI::paramtype prot = 0;
-  vmi->unpack_syscall(&addr, &len, &prot);
-
-  assert(page_aligned<guestptr_t>(addr) && "mprotect address must be page aligned");
-  if(!vmi->get_heap_manager().address_mapped(addr)) {
-    vmi->get_heap_manager().dump_mappings();
-    vmi->get_region_manager()->dump_regions();
-    INFO() <<"mprotect with invalid address: 0x" << std::hex
-      << addr << std::endl;
-    return -EINVAL;
-  }
-
-  Elkvm::Mapping &mapping = vmi->get_heap_manager().find_mapping(addr);
-  int err = 0;
-
-  assert(mapping.get_length() >= len);
-  if(mapping.get_length() != len) {
-    /* we need to split this mapping */
-    vmi->get_heap_manager().slice(mapping, addr, len);
-    vmi->get_heap_manager().create_mapping(addr, len, prot, mapping.get_flags(),
-        mapping.get_fd(), mapping.get_offset());
-  } else {
-    /* only modify this mapping */
-    mapping.mprotect(prot);
-    err = vmi->get_heap_manager().map(mapping);
-  }
-
-  if(vmi->debug_mode()) {
-    DBG() << "MPROTECT requested with address 0x"
-          << std::hex << addr
-          << " len: 0x" << len
-          << " prot: 0x" << prot;
-    print(std::cout, mapping);
-    DBG() << "RESULT: " << err;
-  }
-
-  return err;
-}
-
 long elkvm_do_munmap(Elkvm::VM * vmi) {
   std::shared_ptr<Elkvm::VCPU> vcpu = vmi->get_vcpu(0);
 
