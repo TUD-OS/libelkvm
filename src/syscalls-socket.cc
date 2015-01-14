8586c293b4a290e06624e0118e24298fefb2b077
Björn Döbel <bjoern.doebel@gmail.com>
2015-01-14 16:33:02 +0100

    restructure syscall handler implementations
    multiple files, their names start with syscalls-

---
+++ src/syscalls-socket.cc
@@ -0,0 +1,85 @@
+#include <elkvm/elkvm.h>
+#include <elkvm/elkvm-log.h>
+
+#include <errno.h>
+
+long elkvm_do_sendfile(Elkvm::VM * vmi __attribute__((unused))) {
+  ERROR() << "unimplemented"; exit(1);
+  return -ENOSYS;
+}
+
+long elkvm_do_socket(Elkvm::VM * vmi __attribute__((unused))) {
+  ERROR() << "unimplemented"; exit(1);
+  return -ENOSYS;
+}
+
+long elkvm_do_connect(Elkvm::VM * vmi __attribute__((unused))) {
+  ERROR() << "unimplemented"; exit(1);
+  return -ENOSYS;
+}
+
+long elkvm_do_accept(Elkvm::VM * vmi __attribute__((unused))) {
+  ERROR() << "unimplemented"; exit(1);
+  return -ENOSYS;
+}
+
+long elkvm_do_sendto(Elkvm::VM * vmi __attribute__((unused))) {
+  ERROR() << "unimplemented"; exit(1);
+  return -ENOSYS;
+}
+
+long elkvm_do_recvfrom(Elkvm::VM * vmi __attribute__((unused))) {
+  ERROR() << "unimplemented"; exit(1);
+  return -ENOSYS;
+}
+
+long elkvm_do_sendmsg(Elkvm::VM * vmi __attribute__((unused))) {
+  ERROR() << "unimplemented"; exit(1);
+  return -ENOSYS;
+}
+
+long elkvm_do_recvmsg(Elkvm::VM * vmi __attribute__((unused))) {
+  ERROR() << "unimplemented"; exit(1);
+  return -ENOSYS;
+}
+
+long elkvm_do_shutdown(Elkvm::VM * vmi __attribute__((unused))) {
+  ERROR() << "unimplemented"; exit(1);
+  return -ENOSYS;
+}
+
+long elkvm_do_bind(Elkvm::VM * vmi __attribute__((unused))) {
+  ERROR() << "unimplemented"; exit(1);
+  return -ENOSYS;
+}
+
+long elkvm_do_listen(Elkvm::VM * vmi __attribute__((unused))) {
+  ERROR() << "unimplemented"; exit(1);
+  return -ENOSYS;
+}
+
+long elkvm_do_getsockname(Elkvm::VM * vmi __attribute__((unused))) {
+  ERROR() << "unimplemented"; exit(1);
+  return -ENOSYS;
+}
+
+long elkvm_do_getpeername(Elkvm::VM * vmi __attribute__((unused))) {
+  ERROR() << "unimplemented"; exit(1);
+  return -ENOSYS;
+}
+
+long elkvm_do_socketpair(Elkvm::VM * vmi __attribute__((unused))) {
+  ERROR() << "unimplemented"; exit(1);
+  return -ENOSYS;
+}
+
+long elkvm_do_setsockopt(Elkvm::VM * vmi __attribute__((unused))) {
+  ERROR() << "unimplemented"; exit(1);
+  return -ENOSYS;
+}
+
+long elkvm_do_getsockopt(Elkvm::VM * vmi __attribute__((unused))) {
+  ERROR() << "unimplemented"; exit(1);
+  return -ENOSYS;
+}
+
