a8f964a413cd1f4ec3ca8d8ce61de8fc13cbf580
Florian Pester <florian.pester@tu-dresden.de>
2015-02-10 14:35:10 +0100

    cleanup

--- src/syscalls-clock.cc
+++ src/syscalls-clock.cc
@@ -3,25 +3,26 @@
 
 #include <time.h>
 
-long elkvm_do_clock_gettime(Elkvm::VM * vmi) {
-  if(vmi->get_handlers()->clock_gettime == NULL) {
+long elkvm_do_clock_gettime(Elkvm::VM * vm) {
+  if(vm->get_handlers()->clock_gettime == NULL) {
     ERROR() << "CLOCK GETTIME handler not found" << LOG_RESET << "\n";
     return -ENOSYS;
   }
 
   CURRENT_ABI::paramtype clk_id = 0x0;
   CURRENT_ABI::paramtype tp_p = 0x0;
-  struct timespec *tp = NULL;
+  struct timespec *tp = nullptr;
 
-  vmi->unpack_syscall(&clk_id, &tp_p);
+  vm->unpack_syscall(&clk_id, &tp_p);
   assert(tp_p != 0x0);
 
-  tp = reinterpret_cast<struct timespec *>(vmi->get_region_manager()->get_pager().get_host_p(tp_p));
-  assert(tp != NULL);
+  tp = static_cast<struct timespec *>(vm->host_p(tp_p));
+  assert(tp != nullptr);
 
-  long result = vmi->get_handlers()->clock_gettime(clk_id, tp);
-  if(vmi->debug_mode()) {
-    DBG() << "CLOCK GETTIME with clk_id " << clk_id << " tp " << LOG_GUEST_HOST(tp_p, tp);
+  long result = vm->get_handlers()->clock_gettime(clk_id, tp);
+  if(vm->debug_mode()) {
+    DBG() << "CLOCK GETTIME with clk_id: " << clk_id
+          << " tp: " << LOG_GUEST_HOST(tp_p, tp);
     Elkvm::dbg_log_result<int>(result);
   }
   return result;
